ae624d0c17274ec2d5c1282035a11f2d03b6f812
thatOneArchUser <69575053+thatOneArchUser@users.noreply.github.com>
2022-08-27 10:10:39 +0300

    Create bootloader.asm

---
+++ bootloader.asm
@@ -0,0 +1,22 @@
+; hello world bootloader written in assembly
+
+org 0x7C00
+bits 16
+
+start:
+         cli
+         mov si, msg
+         mov ah, 0x0E ; no skybloc
+
+.loop    lodsb
+         or al, al
+         jz halt
+         int 0x10
+         jmp .loop
+
+halt:    hlt
+
+msg:     db "Hello world!", 0
+
+         times 510 - ($-$$) db 0
+         dw 0xAA55
